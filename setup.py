34e9cc5dd3b6aae8ff8acc2428dfc625a53349f7
jawahar <jawahar273@gmail.com>
2017-12-24 17:05:33 +0530

    remove readme.html

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # from distutils.command.install import INSTALL_SCHEMES
 from setuptools import setup, find_packages
 
-with open('README.html') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
