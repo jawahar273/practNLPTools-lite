9ddeb9dabea4179b4a365fc53d42a1391f7f8122
jawahar273 <jawahar273@gmail.com>
2019-02-08 23:04:29 +0530

    adding config jeklly

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.1',
+    VERSION: '__version__ = &#39;0.3.4&#39;',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
